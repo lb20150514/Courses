6299a84f7eb3ba6fb7599d3c80b44b5e1ac40992
Siren-Blue <32129456+Siren-Blue@users.noreply.github.com>
2018-09-26 16:26:31 +0800

    Update test.cpp

--- VE281/Project-1/test.cpp
+++ VE281/Project-1/test.cpp
@@ -1,1 +1,18 @@
+int partition_inplace(int vec[], int left, int right){
+  swap(vec, (int)(left + right) / 2, left);
+  int pivotat = left, i = left + 1; j = right;
+  while (i < j){
+    while (vec[i] < pivot && i < right) i++;
+    while (vec[j] >= pivot && j > left + 1) j--;
+    if (i < j) swap(vec, i, j);
+  }
+  swap(vec, left, j);
+  return j;
+}
 
+void quicksort_inplace(int vec[], int left, int right){
+  if (left >= right) return;
+  int pivotat = partition_inplace(vec, left, right);
+  quicksort_inplace(vec, left, pivotat - 1);
+  quicksort_inplace(vec, pivotat +1, right);
+}
