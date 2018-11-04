ff04ffc37a74d071f0142f61aa462fdee2e3cddc
Siren-Blue <32129456+Siren-Blue@users.noreply.github.com>
2018-11-04 23:46:51 +0800

    Delete MUX32.v

--- VE370/Project-2/MUX32.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps
-
-module MUX32(IN1, IN2, CONTROL, OUT);
-input wire [31:0] IN1, IN2;
-input wire CONTROL;
-output wire [31:0] OUT;
-    
-assign OUT = CONTROL ? IN1 : IN2;
-endmodule
