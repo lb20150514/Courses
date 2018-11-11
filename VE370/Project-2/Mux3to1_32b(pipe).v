ed69527cf07d44cf59411a34599f41103f26b6c7
lb20150514 <44771805+lb20150514@users.noreply.github.com>
2018-11-11 22:52:54 +0800

    Update Mux3to1_32b(pipe).v

--- VE370/Project-2/Mux3to1_32b(pipe).v
+++ VE370/Project-2/Mux3to1_32b(pipe).v
@@ -5,7 +5,7 @@ input [1:0] sel;
 output [31:0]out;
 
 assign out = 	(sel == 2'b00) ? in1 :
-		          (sel == 2'b01) ? in2 : 
-		          (sel == 2'b10) ? in3 : 32'bx;
+		(sel == 2'b01) ? in2 : 
+		(sel == 2'b10) ? in3 : 32'bx;
 
 endmodule
